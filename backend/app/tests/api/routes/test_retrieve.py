b78045e5443752c07ba175b5c5d7eb7727fc2aa9
SimoTw <simohsiehtw@gmail.com>
2024-11-08 15:24:43 +0800

    fix typo

--- backend/app/tests/api/routes/test_retrieve.py
+++ backend/app/tests/api/routes/test_retrieve.py
@@ -10,7 +10,7 @@ from app.tests.utils.item import create_random_item
 def test_create_item(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
-    data = {"patentId": "US-RE49889-E1", "company_name": "Walmart"}
+    data = {"patent_id": "US-RE49889-E1", "company_name": "Walmart"}
     response = client.post(
         f"{settings.API_V1_STR}/retrieve/infringement-check",
         headers=superuser_token_headers,
